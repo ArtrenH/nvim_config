5b4306ec1988d5db6ea8f23e9a1ed90f7ec2f832
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2023-02-06 22:39:40 +0100

    now cool welcome message trust

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -16,25 +16,6 @@ function M.setup()
       [[ ██║ ╚███║  ╚██╔╝  ██╔╝╚██╗   ██║   ███████╗██║ ╚═╝ ██║ ]],
       [[ ╚═╝  ╚══╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝     ╚═╝ ]],
     }
-    return {
-      [[                                           bbbbbbbb            ]],
-      [[                                           b::::::b            ]],
-      [[                                           b::::::b            ]],
-      [[                                           b::::::b            ]],
-      [[                                            b:::::b            ]],
-      [[nnnn  nnnnnnnn    vvvvvvv           vvvvvvv b:::::bbbbbbbbb    ]],
-      [[n:::nn::::::::nn   v:::::v         v:::::v  b::::::::::::::bb  ]],
-      [[n::::::::::::::nn   v:::::v       v:::::v   b::::::::::::::::b ]],
-      [[nn:::::::::::::::n   v:::::v     v:::::v    b:::::bbbbb:::::::b]],
-      [[  n:::::nnnn:::::n    v:::::v   v:::::v     b:::::b    b::::::b]],
-      [[  n::::n    n::::n     v:::::v v:::::v      b:::::b     b:::::b]],
-      [[  n::::n    n::::n      v:::::v:::::v       b:::::b     b:::::b]],
-      [[  n::::n    n::::n       v:::::::::v        b:::::b     b:::::b]],
-      [[  n::::n    n::::n        v:::::::v         b:::::bbbbbb::::::b]],
-      [[  n::::n    n::::n         v:::::v          b::::::::::::::::b ]],
-      [[  n::::n    n::::n          v:::v           b:::::::::::::::b  ]],
-      [[  nnnnnn    nnnnnn           vvv            bbbbbbbbbbbbbbbb   ]],
-    }
   end
 
   dashboard.section.header.val = header()
