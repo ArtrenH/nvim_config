52f42b8345adcecd7467636a6f5506f630d24885
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2023-02-06 22:13:04 +0100

    test 4

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,6 +18,7 @@ function M.setup()
   local function packer_init()
     local fn = vim.fn
     local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    print(install_path)
     if fn.empty(fn.glob(install_path)) > 0 then
       packer_bootstrap = fn.system {
         "git",
