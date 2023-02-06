37469a301f49966e17807a6c1d485d8e4db11315
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2023-02-06 22:49:30 +0100

    small change

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -29,8 +29,8 @@ function M.setup()
   local function footer()
     -- Number of plugins
     local total_plugins = #vim.tbl_keys(packer_plugins)
-    local datetime = os.date "%d-%m-%Y  %H:%M:%S"
-    local plugins_text = "\t" .. total_plugins .. " plugins  " .. datetime
+    local datetime = os.date "%d-%m-%Y %H:%M:%S"
+    local plugins_text = "\t" .. total_plugins .. " plugins " .. datetime
 
     -- Quote
     local fortune = require "alpha.fortune"
