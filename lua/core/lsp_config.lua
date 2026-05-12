cd1815ffdf3de34725e906b15c2f40e302a328ea
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2026-05-12 16:07:04 +0200

    update

--- lua/core/lsp_config.lua
+++ lua/core/lsp_config.lua
@@ -8,57 +8,55 @@ require('mason').setup({
     }
 })
 require('mason-lspconfig').setup {
-	ensure_installed = {
-		--"bash_language_server",
-		--"codelldb",
-		--"css_lsp",
-		--"html_lsp",
-		--"json_lsp",
-		--"lua-language-server",
-		"pyright",
-		"rust_analyzer",
-		--"svelte-language-server",
-		"texlab",
-		--"typescript-language-server",
-		--"vim-language-server"
-	},
-	automatic_installation = true,
+    ensure_installed = {
+        --"bash_language_server",
+        --"codelldb",
+        --"css_lsp",
+        --"html_lsp",
+        --"json_lsp",
+        --"lua-language-server",
+        "pyright",
+        "rust_analyzer",
+        --"svelte-language-server",
+        "texlab",
+        --"typescript-language-server",
+        --"vim-language-server"
+    },
+    automatic_installation = true,
 }
 
 local on_attach = function(_, _)
-	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-	--vim.keymap.set('n', 'gr', require('telescobe.builtin').lsp_refereces, {})
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+    --vim.keymap.set('n', 'gr', require('telescobe.builtin').lsp_refereces, {})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
 local lspconfig = require('lspconfig');
 
 lspconfig.rust_analyzer.setup {
-	on_attach = on_attach
+    on_attach = on_attach
 }
 
 lspconfig.texlab.setup {
-	on_attach = on_attach
+    on_attach = on_attach
 }
 
 lspconfig.pyright.setup {
-	on_attach = on_attach
+    on_attach = on_attach
 }
 
 lspconfig.emmet_ls.setup({
-	capabilities = capabilities,
-	filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "jinja" },
-	init_options = {
-		html = {
-			options = {
-				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-				["bem.enabled"] = true,
-			},
-		},
-	}
+    capabilities = capabilities,
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "jinja" },
+    init_options = {
+        html = {
+            options = {
+                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                ["bem.enabled"] = true,
+            },
+        },
+    }
 })
-
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,167 +1,169 @@
 local M = {}
 
 function M.setup()
-  -- Indicate first time installation
-  local packer_bootstrap = false
+    -- Indicate first time installation
+    local packer_bootstrap = false
 
-  -- packer.nvim configuration
-  local conf = {
-    display = {
-      open_fn = function()
-        return require("packer.util").float { border = "rounded" }
-      end,
-    },
-  }
-
-  -- Check if packer.nvim is installed
-  -- Run PackerCompile if there are changes in this file
-  local function packer_init()
-    local fn = vim.fn
-    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-      packer_bootstrap = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-      }
-      vim.cmd [[packadd packer.nvim]]
-    end
-    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
-  end
-
-  -- Plugins
-  local function plugins(use)
-    use { "wbthomason/packer.nvim" }
-
-    -- Colorscheme
-    use {
-      "projekt0n/github-nvim-theme", tag = 'v0.0.7',
-      config = function()
-        require('github-theme').setup({
-            theme_style = "dark",
-          })
-        end,
+    -- packer.nvim configuration
+    local conf = {
+        display = {
+            open_fn = function()
+                return require("packer.util").float { border = "rounded" }
+            end,
+        },
     }
 
-    -- Startup screen
-    use {
-      "goolord/alpha-nvim",
-      config = function()
-        require("plugin_config.alpha").setup()
-      end,
-    } --]]
-
-    -- Git
-    use {
-      "TimUntersberger/neogit",
-      requires = "nvim-lua/plenary.nvim",
-      config = function()
-        require("plugin_config.neogit").setup()
-      end,
-    }
-
-		-- basic Plugins
-		use 'jiangmiao/auto-pairs'
-		use 'gi1242/vim-tex-autoclose'
-		-- Vim Plugins
-		use {'lervag/vimtex'}
-		-- Snippets
-		--use {'sirver/ultisnips'}
-		--use({
-			--"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			--tag = "v<CurrentMajor>.*",
-			-- install jsregexp (optional!:).
-			--run = "make install_jsregexp"
-		--})
-
-		use {'sirver/ultisnips'}
-		use {'ckunte/latex-snippets-vim'}
-
-
-		-- general lsp config (language server protocol)
-		use {'neovim/nvim-lspconfig'}
-    -- Rust config
-    use {'williamboman/mason.nvim'}
-    use {'williamboman/mason-lspconfig.nvim'}
-    use {'simrat39/rust-tools.nvim'}
-
-		use {'kyazdani42/nvim-tree.lua'}
-		use {'preservim/tagbar'}
-		use {'folke/todo-comments.nvim'}
-		use {
-			"folke/trouble.nvim",
-			requires = "nvim-tree/nvim-web-devicons",
-			config = function()
-				require("trouble").setup {}
-			end
-		}
-		-- Completion framework:
-    use 'hrsh7th/nvim-cmp'
-
-    -- LSP completion source:
-    use 'hrsh7th/cmp-nvim-lsp'
-
-    -- Useful completion sources:
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/vim-vsnip'
-
-		use {
-				'nvim-treesitter/nvim-treesitter',
-				run = ':TSUpdate',
-				config = function()
-					require("nvim-treesitter.configs").setup {
-						ensure_installed = "python",
-						highlight = {
-							enable = true,
-						}
-					}
-				end
-		}
-    -- lua language server
-    use {"folke/neodev.nvim"}
-		-- python library highlighting
-		use {'numirias/semshi', ft = 'python', run = ':UpdateRemotePlugins',
-			config = function()
-			end
-		}
-		-- markdown
-		use {'ixru/nvim-markdown'}
-		use {'iamcco/markdown-preview.nvim'}
-
-		-- image viewer
-		use {'edluffy/hologram.nvim'}
-		
-		use {
-			'nvim-lualine/lualine.nvim',
-			requires={'nvim-tree/nvim-web-devicons', opt=true},
-			config = function()
-				require("lualine").setup(
-					{}
-				)
-			end
-		}
-
-
-    if packer_bootstrap then
-      print "Restart Neovim required after installation!"
-      require("packer").sync()
+    -- Check if packer.nvim is installed
+    -- Run PackerCompile if there are changes in this file
+    local function packer_init()
+        local fn = vim.fn
+        local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+        if fn.empty(fn.glob(install_path)) > 0 then
+            packer_bootstrap = fn.system {
+                "git",
+                "clone",
+                "--depth",
+                "1",
+                "https://github.com/wbthomason/packer.nvim",
+                install_path,
+            }
+            vim.cmd [[packadd packer.nvim]]
+        end
+        vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
     end
-  end
 
-  packer_init()
+    -- Plugins
+    local function plugins(use)
+        use { "wbthomason/packer.nvim" }
 
-  local packer = require "packer"
-  packer.init(conf)
-  packer.startup(plugins)
+        -- Colorscheme
+        use {
+            "projekt0n/github-nvim-theme", tag = 'v0.0.7',
+            config = function()
+                require('github-theme').setup({
+                    theme_style = "dark",
+                })
+            end,
+        }
 
+        -- Startup screen
+        use {
+            "goolord/alpha-nvim",
+            config = function()
+                require("plugin_config.alpha").setup()
+            end,
+        } --]]
+
+        -- Git
+        use {
+            "TimUntersberger/neogit",
+            requires = "nvim-lua/plenary.nvim",
+            config = function()
+                require("plugin_config.neogit").setup()
+            end,
+        }
+
+        -- basic Plugins
+        use 'jiangmiao/auto-pairs'
+        use 'gi1242/vim-tex-autoclose'
+        -- Vim Plugins
+        use { 'lervag/vimtex' }
+        -- Snippets
+        --use {'sirver/ultisnips'}
+        --use({
+        --"L3MON4D3/LuaSnip",
+        -- follow latest release.
+        --tag = "v<CurrentMajor>.*",
+        -- install jsregexp (optional!:).
+        --run = "make install_jsregexp"
+        --})
+
+        use { 'sirver/ultisnips' }
+        use { 'ckunte/latex-snippets-vim' }
+
+
+        -- general lsp config (language server protocol)
+        use {
+            'neovim/nvim-lspconfig',
+            tag = 'v0.11.0'
+        }
+        -- Rust config
+        use { 'williamboman/mason.nvim' }
+        use { 'williamboman/mason-lspconfig.nvim' }
+        use { 'simrat39/rust-tools.nvim' }
+
+        use { 'kyazdani42/nvim-tree.lua' }
+        use { 'preservim/tagbar' }
+        use { 'folke/todo-comments.nvim' }
+        use {
+            "folke/trouble.nvim",
+            requires = "nvim-tree/nvim-web-devicons",
+            config = function()
+                require("trouble").setup {}
+            end
+        }
+        -- Completion framework:
+        use 'hrsh7th/nvim-cmp'
+
+        -- LSP completion source:
+        use 'hrsh7th/cmp-nvim-lsp'
+
+        -- Useful completion sources:
+        use 'hrsh7th/cmp-nvim-lua'
+        use 'hrsh7th/cmp-nvim-lsp-signature-help'
+        use 'hrsh7th/cmp-vsnip'
+        use 'hrsh7th/cmp-path'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/vim-vsnip'
+
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = ':TSUpdate',
+            config = function()
+                require("nvim-treesitter.configs").setup {
+                    ensure_installed = "python",
+                    highlight = {
+                        enable = true,
+                    }
+                }
+            end
+        }
+        -- lua language server
+        use { "folke/neodev.nvim" }
+        -- python library highlighting
+        use { 'numirias/semshi', ft = 'python', run = ':UpdateRemotePlugins',
+            config = function()
+            end
+        }
+        -- markdown
+        use { 'ixru/nvim-markdown' }
+        use { 'iamcco/markdown-preview.nvim' }
+
+        -- image viewer
+        use { 'edluffy/hologram.nvim' }
+
+        use {
+            'nvim-lualine/lualine.nvim',
+            requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+            config = function()
+                require("lualine").setup(
+                    {}
+                )
+            end
+        }
+
+
+        if packer_bootstrap then
+            print "Restart Neovim required after installation!"
+            require("packer").sync()
+        end
+    end
+
+    packer_init()
+
+    local packer = require "packer"
+    packer.init(conf)
+    packer.startup(plugins)
 end
 
 return M
